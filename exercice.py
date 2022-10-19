2db377360d0fd5b3e6bcc8e81026440b4e6d158a
fabricerenard12 <fabrice.renard12@outlook.com>
2022-10-19 11:12:26 -0400

    Exercices complétés

--- exercice.py
+++ exercice.py
@@ -5,43 +5,89 @@
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
-
-    return []
+        values = []
+        while len(values) < 10:
+            values.append(input('Add value:'))
+        
+    values.sort()
+    return values
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
-        # TODO: demander les mots ici
-        pass
+        words = []
+        while len(words) < 2:
+            words.append(str(input('Add word:')))
+    
 
-    return False
+
+    return (sorted(words[0]) == sorted(words[1]))
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    if len(items) == len(set(items)):
+        return False
+    return True
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    for k, v in student_grades.items():
+        student_grades[k] = sum(v) / len(v)
+    best_student = max(student_grades.items(), key = lambda x: x[1])
+
+    return {best_student[0]: best_student[1]}
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    sentence_dict = {}
 
-    return {}
+    for i in sentence:
+        sentence_dict[i] = sentence.count(i)
+    
+
+    return sentence_dict
 
 
 def get_recipes():
-    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    # TODO: Demander le nom d'une recette, puis ses  et enregistrer dans une structure de données
+    recettes_list = []
+
+    while True:
+        recette_dict = {'Nom': None, 'Ingrédients': None}
+        nom = str(input('Entrez le nom de la recette: '))
+
+        if not len(nom):
+            break
+
+        recette_dict['Nom'] = nom
+        ingrédients = []
+
+        while True:
+            ingrédient = str(input('Entrez le nom d\'un ingrédient: '))
+
+            if not len(ingrédient):
+                break
+
+            ingrédients.append(ingrédient)
+
+        recette_dict['Ingrédients'] = ingrédients
+        recettes_list.append(recette_dict)
+    
+    return recettes_list
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    nom = str(input('Entrez le nom d\'une recette: '))
+
+    for i in ingredients:
+        if i['Nom'] == nom:
+            return i['Ingrédients']
+
+    return None
 
 
 def main() -> None:
